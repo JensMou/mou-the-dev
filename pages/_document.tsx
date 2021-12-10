193f144d1badff8c723eeb8550408d8531872526
Jens Mouridtsen <jlm@web2media.dk>
2021-12-11 00:37:40 +0100

    CHange of std template

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,47 +1,15 @@
-// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-import Document, { Html, Head, Main, NextScript } from "next/document";
-
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="language" content="english" />
-          <meta httpEquiv="content-type" content="text/html" />
-          <meta name="author" content="Jens Mou" />
-          <meta name="designer" content="Jens Mou" />
-          <meta name="publisher" content="Jens Mou" />
-          <meta
-            name="no-email-collection"
-            content="http://www.unspam.com/noemailcollection/"
-          />
-          <meta
-            name="description"
-            content="This is the personal page for Jens Mou - A Web Weveloper"
-          />
-
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin={"crossorigin"}
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-        <footer>Hello1</footer>
       </Html>
-    );
+    )
   }
 }
-
-export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,58 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+import Post from '../types/post'
 
-const Home: NextPage = () => {
+type Props = {
+  allPosts: Post[]
+}
+
+const Index = ({ allPosts }: Props) => {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
   return (
     <>
-      <Head>
-        <title>Mou The Dev - Home</title>
-      </Head>
-
-      <main>
-        <h1>hello</h1>
-        <p>hello</p>
-      </main>
+      <Layout>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Index
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
