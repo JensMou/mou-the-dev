05e6de08843cd39b69ea31ae53cc5ec7ff51f603
Jens Mouridtsen <jlm@web2media.dk>
2021-12-30 11:36:15 +0100

    feat: added simple 404

---
+++ pages/404.tsx
@@ -0,0 +1,42 @@
+import Container from "../components/container";
+import Intro from "../components/intro";
+import Layout from "../components/layout/layout";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const About = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Mou Dev - 404</title>
+        </Head>
+        <Container>
+          <Intro title="404" />
+
+          <h2 className="text-2xl my-5">Page could not be found</h2>
+          <ul>
+            <li>
+              <Link href="/">
+                <a className="hover:text-primary">{"->"} Go to Hello</a>
+              </Link>
+            </li>
+            <li>
+              <p
+                onClick={() => router.back()}
+                className="cursor-pointer hover:text-primary"
+              >
+                {"->"} Go Back
+              </p>
+            </li>
+          </ul>
+        </Container>
+      </Layout>
+    </>
+  );
+};
+
+export default About;
