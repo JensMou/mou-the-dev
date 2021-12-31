bcb047609fe123006449daa3badbcbb1ada5588c
Jens Mouridtsen <jlm@web2media.dk>
2021-12-31 01:02:25 +0100

    feat: working buger menu

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -1,9 +1,19 @@
+import cn from "classnames";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav className="container flex justify-end px-4 py-8 mx-auto">
-      <div className="hidden space-x-8 lg:flex">
+      <div
+        className={cn(
+          "text-xl space-x-6 flex absolute right-16 md:bg-transparent md:space-x-8 md:static md:visible ",
+          {
+            invisible: !open,
+          }
+        )}
+      >
         <Link href="/">
           <a className="hover:text-primary">Hello</a>
         </Link>
@@ -14,10 +24,10 @@ const Navigation = () => {
           <a className="hover:text-primary">About</a>
         </Link>
       </div>
-      <div className="flex lg:hidden">
+      <div onClick={() => setOpen(!open)} className="flex md:hidden">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6"
+          className="w-7 h-7"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
