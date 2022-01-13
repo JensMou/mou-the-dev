c735c7908acb6936c48705c17c435f416e9fd3ed
Jens Mouridtsen <jlm@web2media.dk>
2022-01-14 00:06:03 +0100

    feat: changed the text color on dark and added possibility to use more vh in height

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         background: "#e7e7e7",
         foreground: "#FAFAFB",
 
-        textDark: "#FAFAFB",
+        textDark: "#CCCCCC",
         backgroundDark: "#222",
         foregroundDark: "#121212",
 
@@ -27,6 +27,18 @@ module.exports = {
       },
       lineHeight: {
         tight: 1.2,
+      },
+      height: {
+        "10v": "10vh",
+        "20v": "20vh",
+        "30v": "30vh",
+        "40v": "40vh",
+        "50v": "50vh",
+        "60v": "60vh",
+        "70v": "70vh",
+        "80v": "80vh",
+        "90v": "90vh",
+        "100v": "100vh",
       },
       theme: {
         fontFamily: {
