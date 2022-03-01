a32ecc7db7f185cf4c1c36731c6f00ac1ca243c5
Jens Mouridtsen <jlm@web2media.dk>
2022-03-01 21:21:09 +0100

    feat: making anchor links for header elements in markdown

--- components/project/ProjectBody.tsx
+++ components/project/ProjectBody.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import markdownStyles from "./markdown-styles.module.css";
 
 type Props = {
@@ -5,10 +6,32 @@ type Props = {
 };
 
 const ProjectBody = ({ content }: Props) => {
+  const [modifiedContent, setModifiedContent] = useState<string>(() => {
+    let str = content + "";
+    const arr = content.match(/<h2>(.*?)<\/h2>/g) ?? [];
+    arr.forEach((val) => {
+      if (val) {
+        const id = val
+          .replace("<h2>", "")
+          .replace("</h2>", "")
+          .replace(" ", "-")
+          .toLocaleLowerCase();
+        str = str.replace(
+          val,
+          `<a href="#${id}">${val.replace(
+            "<h2>",
+            `<h2 class="hover:underline" id="${id}" name="${id}">`
+          )}</a>`
+        );
+      }
+    });
+    return str;
+  });
+
   return (
     <div
       className={markdownStyles["markdown"]}
-      dangerouslySetInnerHTML={{ __html: content }}
+      dangerouslySetInnerHTML={{ __html: modifiedContent }}
     />
   );
 };
